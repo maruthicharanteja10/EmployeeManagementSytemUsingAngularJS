8cc740a7b9d5162fda3868f8186732dfa3ff1608
maruthicharanteja <124443822+maruthicharanteja10@users.noreply.github.com>
2025-01-31 17:51:13 +0530

    Add files via upload

---
+++ EmployeeManagementSystemWithAngularJs/src/main/java/com/projects/springboot/controller/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.projects.springboot.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projects.springboot.exception.ResourceNotFoundException;
+import com.projects.springboot.model.Employee;
+import com.projects.springboot.repository.EmployeeRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@GetMapping
+	public List<Employee> listEmployees() {
+
+		return employeeRepository.findAll();
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+		return ResponseEntity.ok(employee);
+	}
+
+	// Add new employee
+	@PostMapping
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	// Update employee
+	@PutMapping("/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id).orElseThrow();
+		employee.setFirstname(employeeDetails.getFirstname());
+		employee.setLastname(employeeDetails.getLastname());
+		employee.setEmail(employeeDetails.getEmail());
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+
+	// Delete employee
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow();
+		employeeRepository.delete(employee);
+		return ResponseEntity.noContent().build();
+	}
+}
